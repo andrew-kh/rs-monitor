0ba7ed22ac38dcf84e878bbf85bc2df9b48936e4
andrew-kh <andrey.khuhlin@gmail.com>
2023-07-30 23:13:17 +0300

    upd

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
 
-url = "https://www.oglasi.rs/nekretnine/prodaja-stanova/beograd"
-
+url = "https://www.oglasi.rs/nekretnine/prodaja-stanova/beograd?i=96"
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, "html.parser")
 articles = soup.find_all("article")
+
+env = Environment(loader=FileSystemLoader('./meta/'))
+template = env.get_template('oglasi_schema.json')
