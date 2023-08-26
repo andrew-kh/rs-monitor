8a6529db9e236a3d279a7dac1523eee84909350b
andrew-kh <andrey.khuhlin@gmail.com>
2023-08-26 12:03:13 +0300

    upd function s

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import rsm_utils as rs
 
-ad_object = rs.make_json_template()
+TEMPLATE_PATH='./meta/'
+TEMPLATE_NAME='oglasi_schema.txt'
+
+ad_template = rs.make_json_template(template_path=TEMPLATE_PATH,template_name=TEMPLATE_NAME)
--- rsm_utils.py
+++ rsm_utils.py
@@ -3,7 +3,7 @@ from jinja2 import Environment, FileSystemLoader, Template
 def make_json_template(template_path:str, template_name:str) -> Template:
     """return jinja2 Template to render ad jsons"""
 
-    env = Environment(loader=FileSystemLoader('./meta/'))
-    template = env.get_template('oglasi_schema.txt')
+    env = Environment(loader=FileSystemLoader(template_path))
+    template = env.get_template(template_name)
     
     return template
