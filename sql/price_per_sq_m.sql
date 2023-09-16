330083999348eef2837bca36e1be101e19d81abd
andrew-kh <andrey.khuhlin@gmail.com>
2023-09-16 23:32:31 +0300

    add sql

---
+++ sql/price_per_sq_m.sql
@@ -0,0 +1,10 @@
+-- Active: 1672421927448@@localhost@5432
+select
+	replace(ad_json->'property_info'->>'Kvadratura', 'm2', '')::float area,
+	(split_part(ad_json->>'property_price', ',',1)::float)*1000 price,
+	((split_part(ad_json->>'property_price', ',',1)::float)*1000)
+	/
+	replace(ad_json->'property_info'->>'Kvadratura', 'm2', '')::float price_per_sq_m
+from dev.ads_demo
+where 1=1
+	and source_directory_id is not null;
