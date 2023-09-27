de910737cfc71cd7b7bb50f2444129f5aa044002
andrew-kh <andrey.khuhlin@gmail.com>
2023-09-27 23:25:37 +0300

    test of quarantine

--- load_data_to_pg.py
+++ load_data_to_pg.py
@@ -21,6 +21,8 @@ test_folder_path=f'/usr/project_rs_monitor/data/landing/oglasi/sale/{str(test_fo
 data_files_dir = os.listdir(test_folder_path)
 data_files_list=[file for file in data_files_dir if file.endswith('.json')]
 
+quarantine_path='/usr/project_rs_monitor/data/landing/quarantine/sale/'
+
 num_files_proc=0
 
 for i in data_files_list:
@@ -28,11 +30,12 @@ for i in data_files_list:
     test_file_path=i
     test_file_full_path=os.path.join(test_folder_path, test_file_path)
 
-    with open(test_file_full_path, 'r') as json_file:
-        print(test_file_full_path)
-        json_data = json.load(json_file)
-
     try:
+
+        with open(test_file_full_path, 'r') as json_file:
+            print(test_file_full_path)
+            json_data = json.load(json_file)
+            
         # this works if json_data is a dict, not a string repr of json'
         sql_query = f"INSERT INTO dev.ads_demo (source_directory_id, ad_json) VALUES ({str(test_folder_id)}, '{json.dumps(json_data, ensure_ascii=False)}');"
 
@@ -45,6 +48,9 @@ for i in data_files_list:
         cursor.execute('ROLLBACK')
         connection.commit()
         # print(f'error parsing file {i}')
+
+        new_file_path = os.path.join(quarantine_path, os.path.basename(test_file_full_path))
+        os.rename(test_file_full_path, new_file_path)
 
     if num_files_proc == 500:
         break
