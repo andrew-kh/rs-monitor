96a23ce356bf50a45e6373f7c8ac87e01f3a2838
andrew-kh <andrey.khuhlin@gmail.com>
2023-09-28 22:58:01 +0300

    upd sql

--- sql/price_per_sq_m.sql
+++ sql/price_per_sq_m.sql
@@ -7,3 +7,17 @@ select
 from dev.ads_demo
 where 1=1
 	and source_directory_id is not null
+
+
+
+select
+	ad_json->>'property_district',
+	sum(split_part(ad_json->>'property_price', ',',1)::float)/count(ad_json->>'property_district') av_price,
+	sum(((split_part(ad_json->>'property_price', ',',1)::float)*1000)
+	/
+	replace(ad_json->'property_info'->>'Kvadratura', 'm2', '')::float)/count(ad_json->>'property_district') av_price_per_sq_m
+from dev.ads_demo
+where 1=1
+	and source_directory_id is not null
+group by 1
+order by 3 desc
