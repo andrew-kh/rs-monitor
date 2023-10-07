9499c4335899799fcf0b2ca98fcb792bd5c9dc78
andrew-kh <andrey.khuhlin@gmail.com>
2023-10-07 23:37:01 +0300

    uod

--- test.py
+++ test.py
@@ -27,21 +27,32 @@ print(parsing_dir)
 if not os.path.exists(parsing_dir):
     os.makedirs(parsing_dir)
 
-ads = soup.find_all("article")
 
-ad_links = []
-for ad in ads:
-    ad_link_object = ad.find_all('a', class_='fpogl-list-title')
-    ad_link = ad_link_object[0]['href']
-    ad_links.append(WEBSITE+ad_link)
+#####
+for page_number in range(1,num_pages+1):
+
+    ads = soup.find_all("article")
+
+    ad_links = []
+    for ad in ads:
+        ad_link_object = ad.find_all('a', class_='fpogl-list-title')
+        ad_link = ad_link_object[0]['href']
+        ad_links.append(WEBSITE+ad_link)
 
 
-with Pool(10) as p:
-    result_list = p.map(rs.parse_ad_page, ad_links)
+    with Pool(10) as p:
+        result_list = p.map(rs.parse_ad_page, ad_links)
 
 
-for ad_object, file_name in zip(result_list, ad_links):
-    json_name=file_name.replace(WEBSITE,'')[1:].replace('/','_')+'_'+str(int(time.time()))+'.json'
-    file_path=parsing_dir+json_name[:200]
-    with open(file_path, "w") as json_file:
-        json_file.write(ad_object)
+    for ad_object, file_name in zip(result_list, ad_links):
+        json_name=file_name.replace(WEBSITE,'')[1:].replace('/','_')+'_'+str(int(time.time()))+'.json'
+        file_path=parsing_dir+json_name[:200]
+        with open(file_path, "w") as json_file:
+            json_file.write(ad_object)
+
+    time.sleep(random.uniform(0.5, 1.2))
+
+    ad_list_path = f'/nekretnine/prodaja-stanova/beograd?p={page_number}&i=96'
+    ad_list_url = meta_website + ad_list_path
+    response = requests.get(ad_list_url)
+    soup = BeautifulSoup(response.text, "html.parser")
